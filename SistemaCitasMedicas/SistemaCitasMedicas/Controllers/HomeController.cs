5b73481aed484f3941fc5236c5b7fc078ef50ccb
Adolfo Planchart <47159444+AdolfoPlanchart@users.noreply.github.com>
2021-11-09 23:05:03 -0500

    Login with session object for user

--- SistemaCitasMedicas/SistemaCitasMedicas/Controllers/HomeController.cs
+++ SistemaCitasMedicas/SistemaCitasMedicas/Controllers/HomeController.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistemaCitasMedicas.DAO;
+using SistemaCitasMedicas.Models;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace SistemaCitasMedicas.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult SessionTests()
+        {
+            return View(Session["usuario"] as List<Usuario>);
+        }
+        usuarioDAO usuarios = new usuarioDAO();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string clave)
+        {
+            Usuario usuario = usuarios.validarLogin(email, clave);
+            
+            if(usuario!=null)
+            { 
+                Session["usuario"] = usuario;
+                return View("SessionTests");
+            }
+            
+            ViewBag.msj = "Error: combinación de correo y clave errónea.";
+            
+            return View("Login");
+        }
         public ActionResult Index()
         {
             return View();
---
+++ SistemaCitasMedicas/SistemaCitasMedicas/DAO/usuarioDAO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using SistemaCitasMedicas.Models;
+
+namespace SistemaCitasMedicas.DAO
+{
+    public class usuarioDAO
+    {
+        conexionDAO cn = new conexionDAO();
+        public Usuario validarLogin(string email, string clave)
+        {
+            return listado().Where(u => u.email == email && u.clave == clave).FirstOrDefault();
+        }
+        public IEnumerable<Usuario> listado()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            cn = new conexionDAO();
+            using(cn.getcn)
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "exec usp_listar_usuarios",
+                    cn.getcn
+                );
+                cn.getcn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while(dr.Read())
+                {
+                    lista.Add(new Usuario
+                    {
+                        idusuario   = (int)dr[0],
+                        dniusu      = (int)dr[1],
+                        nombreusu   = (string)dr[2],
+                        idsexo      = (int)dr[3],
+                        iddistrito  = (int)dr[4],
+                        celularusu  = (string)dr[5],
+                        email       = (string)dr[6],
+                        clave       = (string)dr[7],
+                        fechaRegusu = (DateTime)dr[8],
+                        idtipo      = (int)dr[9],
+                        idestado    = (int)dr[10]
+                    });
+                }
+                dr.Close();
+                cn.getcn.Close();
+            }
+            return lista;
+        }
+    }
+}
